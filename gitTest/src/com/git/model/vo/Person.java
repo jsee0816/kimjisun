912c3a09b77482c360e2c31bf97199d8360e2e48
MSI\jsee0 <jsee0816@gmail.com>
2020-09-15 11:28:08 +0900

    04_Person에 결혼여부, 이메일 추가 - js-

--- gitTest/src/com/git/model/vo/Person.java
+++ gitTest/src/com/git/model/vo/Person.java
@@ -5,8 +5,8 @@ public class Person {
 	private String name;
 	private int age;
 	private String address;
-	private String[] hobby;
-	private String phone;
+	private String email;
+	private boolean married;
 	
 	public Person() {
 		// TODO Auto-generated constructor stub
@@ -42,15 +42,24 @@ public class Person {
 		this.address = address;
 	}
 
-	public String[] getHobby() {
-		return hobby;
-	}
-
-	public void setHobby(String[] hobby) {
-		this.hobby = hobby;
-	}
-
 	
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isMarried() {
+		return married;
+	}
+
+	public void setMarried(boolean married) {
+		this.married = married;
+	}
+
 	public String getPhone() {
 		return phone;
 	}
